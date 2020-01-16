e44bc638474dfc1de92b57eb8913011b7b89edcd
Ayub Mohamud <a.mohamud1@hotmail.com>
2020-01-16 15:48:52 +0000

    identicons moving

--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -3,146 +3,95 @@ import './Game.css';
 import zeroArray from '../../utils/zeroArray'
 import buildIdenticon from '../../utils/buildIdenticon'
 
+const followers = ['svnmmrs','samhstn', 'nikkesan', 'hajimon54', 'albadylic', 'redahaq' , 'pat-cki' , 'bethanyios' , 'tonylomax' , 'crianonim']
+
 const Game = props => {
     // hard code data that would be passed in
-    const username = 'Ayub3' // github username submitted at landing
-    const interval = 1000; // time between steps in ms
-    const followers = ['svnmmrs','yeoyeo']
+    const interval = 500; // time between steps in ms
 
     // set up states
-    const [nextIdenticon, setNextIdenticon] = React.useState(buildIdenticon(username))
-    const initialField = zeroArray(5,5)
-    initialField.forEach((lane,i) => { initialField[i] = lane.concat(nextIdenticon[i]) })
-    const [field, setField] = React.useState(initialField) 
+    const [field, setField] = React.useState(() => {
+        const username = 'Ayub3' // github username submitted at landing
+        const initialIdenticon = buildIdenticon(username)
+        const initialField = zeroArray(5,5)
+        initialField.forEach((lane,i) => { initialField[i] = lane.concat(initialIdenticon[i]) })
+        return initialField
+    }) 
+    const [nextIdenticon, setNextIdenticon] = React.useState(() => {
+        return buildIdenticon(followers.shift())
+    })
     const [t, setT] = React.useState(0);
+    const [playerPosition, setPlayerPosition] = React.useState(2)
+    const [score, setScore] = React.useState(0)
 
-    // set up time ticker
-    // const tick = () => { setT(t => t + 1) }
-    // setInterval(tick, interval)
+    // set up timer and event listeners on mount
+    React.useEffect(()=>{
+        const tick = () => { setT(t => t + 1) }
+        const timer = setInterval(tick, interval)
+
+        return () => {
+            clearInterval(timer)
+        }
+    },[])
 
     // run this code every time time ticks
-    // React.useEffect(() => {
-    //     if (t % 10 === 0) {
-    //         setField(field => {
-    //             field.forEach((lane,i) => {
-    //                 const block = lane.shift()
-    //                 if (block) {
-    //                     // run code for player killed ('GAME OVER!')
-    //                 }
-    //                 lane.push(nextIdenticon[i].shift())
-    //             })
-    //         })
-    //         if (followers.length) {
-    //             setNextIdenticon(buildIdenticon(followers.shift()))
-    //         } else {
-    //             // run code to finish the game ('YOU WIN!')
-    //         }
-    //     }
-    //     else if(t % 10 < 6) {
-    //         setField(field => {
-    //             field.forEach((lane,i) => {
-    //                 lane.shift()
-    //                 lane.push(0)
-    //             })
-    //         })
-    //     }
-    //     else if(t % 10 < 10) {
-    //         setField(field => {
-    //             field.forEach((lane,i) => {
-    //                 const block = lane.shift()
-    //                 if (block) {
-    //                     // run code for player killed ('GAME OVER!')
-    //                 }
-    //                 lane.push(nextIdenticon[i].shift())
-    //             })
-    //         })
-    //     }
-    // }, [t])
+    React.useEffect(() => {
+        if (t % 10 === 0 && t !== 0) {
+            setField(field => {
+                return field.map((lane,i) => {
+                    const block = lane.shift()
+                    if (block) {
+                        // run code for player killed ('GAME OVER!')
+                    }
+                    lane.push(nextIdenticon[i].shift())
+                    return lane
+                })
+            })
+            if (followers.length) {
+                setNextIdenticon(buildIdenticon(followers.shift()))
+            } else {
+                // run code to finish the game ('YOU WIN!')
+                console.log(`Game finished. Your score was ${score}!`)
+            }
+        }
+        else if(t % 10 < 6 && t !== 0) {
+            setField(field => {
+                return field.map((lane,i) => {
+                    lane.shift()
+                    lane.push(0)
+                    return lane
+                })
+            })
+        }
+        else if(t % 10 < 10 && t !== 0) {
+            setField(field => {
+                return field.map((lane,i) => {
+                    const block = lane.shift()
+                    if (block) {
+                        // run code for player killed ('GAME OVER!')
+                    }
+                    lane.push(nextIdenticon[i].shift())
+                    return lane
+                })
+            })
+        }
+    }, [t])
 
     return (<div className="game-grid">
         {
             field.map((lane,i) => {
-                console.log(lane)
                 return <div key={i} className="game-grid__column">
                 {lane.map((block,j) => {
                     let reversej = (lane.length-1)-j;
-                    return lane[reversej] ? <div key ={i.toString() + reversej.toString()} className="game-grid__square game-grid__square--active"></div>
-                        : <div className="game-grid__square"></div>
+                    return lane[reversej] ? <div key={i.toString() + reversej.toString()} className="game-grid__square game-grid__square--active"></div>
+                        : <div key={i.toString() + reversej.toString()} className="game-grid__square"></div>
                 })}
                 </div>
             })
         }
-        {/* <div className="game-grid__column">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div>
-        <div className="game-grid__row">
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>
-            <div className="game-grid__square"></div>   
-        </div> */}
     </div>)
+
+  
 }
 
 export default Game
--- src/utils/buildIdenticon.js
+++ src/utils/buildIdenticon.js
@@ -6,14 +6,13 @@ const md5 = require('md5')
 const buildIdenticon = username => {
   const hash = md5(username) // get md5 hash of given username
   let hexes = hash.match(/.{2}/g) // split into 16 numbers (all hex, base 16)
-  console.log('hexes: ', hexes)
   let identicon = zeroArray(5,5) // initialise identicon as empty 5x5 grid
   // fill in identicon grid with hex values tested for even-ness
   // NB. neither of these patterns seem to work (to be sure, consider what else can vary about implementation?)
   // pattern 1 (see readme)
   for (let j=4; j>=0; j--) {
     for (let i=0; i<3; i++) {
-      console.log(`identicon position ${i},${j} takes hex ${hexes[i + (4-j)*3]} at index ${i + (4-j)*3}`)
+      // console.log(`identicon position ${i},${j} takes hex ${hexes[i + (4-j)*3]} at index ${i + (4-j)*3}`)
       identicon[i][j] = parseInt(hexes[i + (4-j)*3],16) % 2 === 0 ? 1 : 0;
     }
   }
@@ -30,7 +29,6 @@ const buildIdenticon = username => {
       identicon[4-i][j] = identicon[i][j]
     }
   }
-  console.log('final identicon: ', identicon)
   return identicon;
 }
 
