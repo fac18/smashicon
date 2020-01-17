13443e64ff2da73bf3e187da31ba24a93bbc026c
Ayub Mohamud <a.mohamud1@hotmail.com>
2020-01-17 12:40:46 +0000

    add test for making a mock response

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render,fireEvent } from '@testing-library/react';
+import { render,fireEvent,act,waitForDomChange } from '@testing-library/react';
 import Form from './Form';
 
 test('check if the form works' , () => {
@@ -7,9 +7,20 @@ const {getByLabelText,getByText, container} = render(<Form/>)
 const inputNode = getByLabelText('Enter your GitHub username')
 fireEvent.change(inputNode, {target : {value:'ayub3'}})
 const slider = container.querySelector('.form__range')
-fireEvent.change(slider , {target : {value:1000}})
+fireEvent.change(slider , {target : {value: 500}})
 const button = getByText('Play!')
-fireEvent.click(button)
+const mockResponse = {username:'ayub3', followers: ['svnmmrs' , 'redahaq'] , interval : 500 , avatar_url : 'https://avatars3.githubusercontent.com/u/50529930?s=460&v=4'}
+global.fetch = jest
+    .fn()
+    .mockImplementation(() =>
+      Promise.resolve({ text: () => Promise.resolve(mockResponse) })
+    );
+    act(()=>{
+        fireEvent.click(button)
+    })
+  
+expect(container.firstChild.className).toBe('game')
 })
+
 
 
