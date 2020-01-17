104569ca373d99a0ce6552d5dc04d56f41df043a
Ayub Mohamud <a.mohamud1@hotmail.com>
2020-01-17 12:05:46 +0000

    add test for from

--- src/components/Form/Form.test.js
+++ src/components/Form/Form.test.js
@@ -1,3 +1,15 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render,fireEvent } from '@testing-library/react';
 import Form from './Form';
+
+test('check if the form works' , () => {
+const {getByLabelText,getByText, container} = render(<Form/>)
+const inputNode = getByLabelText('Enter your GitHub username')
+fireEvent.change(inputNode, {target : {value:'ayub3'}})
+const slider = container.querySelector('.form__range')
+fireEvent.change(slider , {target : {value:1000}})
+const button = getByText('Play!')
+fireEvent.click(button)
+})
+
+
